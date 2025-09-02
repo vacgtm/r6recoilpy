b033b314ca8f291b382ce2bedde1d1bb9e99ef35
vac <smiffnwesson1233@gmail.com>
2025-09-02 18:24:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ from pynput import mouse, keyboard
 from pynput.mouse import Button
 import time
 import threading
-import json, ctypes
+import ctypes, os
+from modules.readjson import read_json as rjv
+
 mouse_controller = mouse.Controller()
 is_pressed = False
 is_left_pressed = False
 is_right_pressed = False
 
 
-
+x_val = 0
+y_val = 0
+speed = 0
+toggle_key = "p"
 
 
 
 toggled = False
-x_val = 0.999
-y_val = 6.9
-speed = 0.025
-toggle_key = "p"
+
 
 def move_mouse(dx, dy):
     ctypes.windll.user32.mouse_event(0x0001, int(dx), int(dy), 0, 0)
@@ -54,14 +56,57 @@ def on_press(key):
             print("toggled")
             toggled = True
 
-threading.Thread(target=recoil_code, daemon=True).start()
 
-mouse_listener = mouse.Listener(on_click=on_click)
-keyboard_listener = keyboard.Listener(on_press=on_press)
-mouse_listener.start()
-keyboard_listener.start()
-mouse_listener.join()
-keyboard_listener.join()
+def init():
+    os.system("cls")
+    print("-- r6 recoil compensator --")
+    print("--- Presets ---\nCustom\nG36C\nMP7")
+    a = input("\n\n\n\n> ")
+    if a.lower() == "g36c":
+        os.system("cls")
+        print("-- g36c preset loading --")
+        time.sleep(2)
+        run_all("g36c")
+    elif a.lower() == "mp7":
+        os.system("cls")
+        print("-- mp7 preset loading --")
+        time.sleep(2)
+        run_all("mp7")
+    elif a.lower() == "custom":
+        os.system("cls")
+        print("-- your custom preset is loading --")
+        time.sleep(2)
+        run_all("custom")
+
+def run_all(pre):
+    global x_val, y_val, speed, toggle_key
+    if pre == "g36c":
+        
+        x_val = 0.999
+        y_val = 6.9
+        speed = 0.025
+        toggle_key = "p"
+    elif pre == "mp7":
+        x_val = 0.05
+        y_val = 7.4
+        speed = 0.02
+        toggle_key = "p"
+    elif pre == "custom":
+        x_val = rjv("configuration/config.json", "x_val")
+        y_val = rjv("configuration/config.json", "y_val")
+        speed = rjv("configuration/config.json", "speed")
+        toggle_key = rjv("configuration/config.json", "toggle_key")
+    
+    os.system("cls")
+    print("Loaded.")
+    threading.Thread(target=recoil_code, daemon=True).start()
+
+    mouse_listener = mouse.Listener(on_click=on_click)
+    keyboard_listener = keyboard.Listener(on_press=on_press)
+    mouse_listener.start()
+    keyboard_listener.start()
+    mouse_listener.join()
+    keyboard_listener.join()
 
 
 
@@ -69,7 +114,7 @@ keyboard_listener.join()
 
 
 
-
+init()
 
 
 
